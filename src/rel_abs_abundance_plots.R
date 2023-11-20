dc9aa92b1698765b19c9d1c5aa8ee83496cf326b
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2023-11-19 22:05:26 -0800

    function to read and process rel abundance files

--- src/rel_abs_abundance_plots.R
+++ src/rel_abs_abundance_plots.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(data.table)
+library(reshape2)
 # read data
 growth_data <- fread("/Users/maryam/mnt/home/Growth vs N and P.csv")
 n_fixing_data <-fread("/Users/maryam/mnt/home/n_fixing.csv")
@@ -12,3 +13,27 @@ growth_data$`[DIN] 10/17` <- scale(growth_data$`[DIN] 10/17`)
 growth_data<-growth_data%>%select(Origin,Variety,`[DIN] 10/17`,RGR)%>%
   group_by(Variety,Origin)%>% summarise(mean_nitrogen = mean(`[DIN] 10/17`),
                                         mean_RGR=mean(RGR))
+# function to read relative abundance files and sum the abundance values
+read_preprocess_file <- function(genus_name) {
+  relative_abundance_data<-as.data.frame(fread(
+    paste("/Users/maryam/mnt/home/",
+    "zr13074.16S_231019.zymo/02...US.vs.NONUS.Bac16Sv34",
+    "/Taxa2ASV_Decomposer/Genus/","g__",
+    genus_name,"/seq.relative.abun.transpose.csv", sep = "")))
+  colsum<- colSums(relative_abundance_data[,-1],na.rm=TRUE)
+  colsum$genus<-genus_name
+  return(colsum)
+}
+# apply the function
+relative_abundance_files <- mapply(read_preprocess_file,
+                                   n_fixing_data$s , SIMPLIFY = FALSE)
+# simplofy the mapply output
+relative_abundance_files_unzipped <- as.data.frame(
+  do.call(rbind,relative_abundance_files))
+# unlist the output
+rel_abun_unlisted<-as.data.frame(unnest(relative_abundance_files_unzipped,
+                                        colnames(relative_abundance_files_unzipped)))
+# move genus column to row names
+rownames(rel_abun_unlisted)<-rel_abun_unlisted$genus
+
+melt_rel_abun<-melt(rel_abun_unlisted[,-22])
