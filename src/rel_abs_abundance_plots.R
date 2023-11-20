a82777162c8e52217206324a07fa3bfad85d1275
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2023-11-19 22:00:26 -0800

    directory for source codes

---
+++ src/rel_abs_abundance_plots.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(data.table)
+# read data
+growth_data <- fread("/Users/maryam/mnt/home/Growth vs N and P.csv")
+n_fixing_data <-fread("/Users/maryam/mnt/home/n_fixing.csv")
+# change origin value to factor data type
+# Normalize the RGR and DIN so they are both in the same ranges
+growth_data$Origin <- as.factor(growth_data$Origin)
+growth_data$Variety <- as.factor(growth_data$Variety)
+growth_data$RGR <-scale(growth_data$RGR)
+growth_data$`[DIN] 10/17` <- scale(growth_data$`[DIN] 10/17`)
+growth_data<-growth_data%>%select(Origin,Variety,`[DIN] 10/17`,RGR)%>%group_by(Variety,Origin)%>% summarise(mean_nitrogen = mean(`[DIN] 10/17`),mean_RGR=mean(RGR))
