6d7f35cba6aea768272eb830b03aeaef3f7497e1
Maryam Ahmadi jeshvaghane <maryam@Maryams-MacBook-Air.local>
2023-11-19 22:02:04 -0800

    read files and change data type and scale continuous values

--- src/rel_abs_abundance_plots.R
+++ src/rel_abs_abundance_plots.R
@@ -3,10 +3,12 @@ library(data.table)
 # read data
 growth_data <- fread("/Users/maryam/mnt/home/Growth vs N and P.csv")
 n_fixing_data <-fread("/Users/maryam/mnt/home/n_fixing.csv")
-# change origin value to factor data type
-# Normalize the RGR and DIN so they are both in the same ranges
+# change origin and variety value to factor data type
 growth_data$Origin <- as.factor(growth_data$Origin)
 growth_data$Variety <- as.factor(growth_data$Variety)
+# Normalize the RGR and DIN so they are both in the same ranges
 growth_data$RGR <-scale(growth_data$RGR)
 growth_data$`[DIN] 10/17` <- scale(growth_data$`[DIN] 10/17`)
-growth_data<-growth_data%>%select(Origin,Variety,`[DIN] 10/17`,RGR)%>%group_by(Variety,Origin)%>% summarise(mean_nitrogen = mean(`[DIN] 10/17`),mean_RGR=mean(RGR))
+growth_data<-growth_data%>%select(Origin,Variety,`[DIN] 10/17`,RGR)%>%
+  group_by(Variety,Origin)%>% summarise(mean_nitrogen = mean(`[DIN] 10/17`),
+                                        mean_RGR=mean(RGR))
